059d3c0bf96de67fdc5c2357f1538104e214b5e3
Mark Soifer <posurlu@gmail.com>
2016-08-08 15:21:21 +0300

    CRUD btns updated

--- app/Buttons/Models/crud-btn.ts
+++ app/Buttons/Models/crud-btn.ts
@@ -1,19 +1,33 @@
 
 export class CrudBtn {
-    constructor(data:any){
-        this.Id = data.Id;
-        this.Type = data.GenreName;
-        switch (data.) {
-            case value:
-                
+    private _type: string;
+    public Id: number;
+    public get Type(): string {
+        console.log("set");
+        return this._type;
+    };
+    public set Type(type): string {
+        console.log("get: " + type);
+        switch (type) {
+            case "hide":
+                this._type = "glyphicon glyphicon-eye-close";
                 break;
-        
+            case "unHide":
+                this._type = "glyphicon glyphicon-eye-open";
+                break;
+            case "edit":
+                this._type = "glyphicon glyphicon-edit";
+                break;
+            case "create":
+                this._type = "glyphicon glyphicon-plus";
+                break;
             default:
                 break;
         }
-    }
-    Id:number;
-    Type:string;
+    };
+
+};
+
 
 }
 
--- app/Vod/genre.component.ts
+++ app/Vod/genre.component.ts
@@ -1,24 +1,24 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import { GenreService } from './services/genre.service';
 import {Genre} from './models/genre';
+import { CrudBtnComponent } from '../Buttons/crud-btn.component';
 
 @Component({
     selector: 'genre',
     templateUrl: 'app/vod/genre.component.html',
-    providers: [GenreService]
+    providers: [GenreService],
+    directives: [CrudBtnComponent]
 })
 
 export class GenreComponent implements OnInit, OnDestroy {
     public genres: Array<Genre>;
     private sub: any;
     constructor(private _service: GenreService) {
-        console.log("genre constructor");
     }
     ngOnInit() {
         console.log("genre init");
         this.sub = this._service.getGenres().subscribe(data => {
             this.genres = data;
-            console.log("genres :" + data);
         });
     }
     ngOnDestroy() {
