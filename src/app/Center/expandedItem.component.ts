a8ec6520bacbfb856dbc72b19cbe44d4366b870e
Mark Soifer <posurlu@gmail.com>
2016-09-05 19:02:31 +0300

    Working modal component

--- src/app/Center/expandedItem.component.ts
+++ src/app/Center/expandedItem.component.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
     selector: 'expandedItem',
-    templateUrl: 'expandedItem.component.html'
+    templateUrl: 'expandedItem.component.html',
+    styles: [`
+        .extendedItemContainer {
+             background-color: rgba(245,245,245,0.9);
+             height:100%;
+            }
+              .extendedItemContainer > md-card {
+               width:800px;
+               margin-left:auto;
+               margin-right:auto;
+               z-index:150;
+ 
+              }
+
+           `]
 })
+
 export class ExpandedCardComponent implements OnInit {
     @Input() item: any;
 
     registerForm: FormGroup;
 
     constructor(private formBuilder: FormBuilder) { }
-
+    @Output() change = new EventEmitter();
+    clearItem() {
+        this.change.emit();
+    }
+   stopPropagation(event)
+   {
+       event.stopPropagation()
+   }
     ngOnInit() {
-
         this.registerForm = this.formBuilder.group({
-            firstname: ['', Validators.required],
+            firstname: [this.item.Name, Validators.required],
             lastname: ['', Validators.required],
             address: this.formBuilder.group({
                 street: [],
@@ -22,6 +43,5 @@ export class ExpandedCardComponent implements OnInit {
                 city: []
             })
         });
-
     }
 }
--- src/app/Center/grid.component.ts
+++ src/app/Center/grid.component.ts
@@ -6,15 +6,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'grid.component.html',
 
   styles: [`
-            md-card{border:1px solid red; min-width:200px; width:300px;}
+            md-card{ min-width:150px; width:258px;height:100px;}
             .card-container{
                             display: flex;
                             flex-flow: row wrap;
+                      
+                                
                           }
              .card-container > md-card{
                             margin: 10px;  
-                            border: 0 1px 1px 0;                          
+                            border: 0 1px 1px 0;                    
                           }
+             .card-container > md-card  md-card-subtitle{
+                    display:inline-block;
+                    width:152px;
+                    white-space: nowrap;
+                    overflow:hidden !important;
+                    text-overflow: ellipsis;
+             }                          
+
              `]
 
 })
@@ -33,12 +43,24 @@ export class GridComponent implements OnInit {
     { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
     ,
     { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
+    ,
+    { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
+    ,
+    { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
+    ,
+    { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
+    ,
+    { 'Id': '5', 'Name': 'Five One', 'Url': 'http://loremflickr.com/320/240', 'isCollapsed': true, 'Text': 'adsfasdf' }
   ];
-  selectedItem:any;
+  selectedItem: any;
   constructor() { }
   onClick(item) {
     this.selectedItem = item;
   }
   ngOnInit() { }
+  clearItem() {
+    this.selectedItem = null;
+    console.log("click");
 
+  }
 }
